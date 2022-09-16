492d3240fcf83badb287e76bf05fbc92d22f30e1
Diego Ripley <diego@diegoripley.ca>
2022-09-16 18:32:58 +0000

    Modify stacks.

--- stacks/build.sh
+++ stacks/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build . -t boc/savy:jammy
+docker build . -t boc/savy-base:jammy --target base
+docker build . -t boc/savy-run:jammy --target run
+docker build . -t boc/savy-build:jammy --target build
