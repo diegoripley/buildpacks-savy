a1e8a0d3db1e0c716ac8f69bcc9a112a771ac633
Diego Ripley <diego@diegoripley.ca>
2022-09-21 18:26:28 +0000

    Add Julia Packages installation script.

---
+++ apps/SaVy.jl/packages.jl
@@ -0,0 +1,38 @@
+using Pkg
+
+dependencies = [
+    "CSV",
+    "ClusterManagers",
+    "ColorSchemes",
+    "Colors",
+    "DataFrames",
+    "Dates",
+    "Distributed",
+    "Distributions",
+    "FAME",
+    "Genie",
+    "JLD2",
+    "JSON",
+    "JSON3",
+    "LENS",
+    "MAT",
+    "ModelBaseEcon",
+    "Nettle",
+    "OrderedCollections",
+    "PackageCompiler",
+    "Plots",
+    "ProgressMeter",
+    "ProjUtils",
+    "Revise",
+    "StateSpaceEcon",
+    "Statistics",
+    "StatsPlots",
+    "StringEncodings",
+    "Suppressor",
+    "TOML",
+    "Test",
+    "TimeSeriesEcon",
+    "XLSX"
+]
+
+Pkg.add(dependencies)
