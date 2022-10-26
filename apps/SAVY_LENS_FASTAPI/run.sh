ab07cad6e4fa01bfa88d71b30a76fb7bc3db6fc9
Diego Ripley <diego@diegoripley.ca>
2022-10-26 14:46:59 +0000

    Update Python buildpacks.

---
+++ apps/SAVY_LENS_FASTAPI/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -L ~/.local ]; then
+    echo "Creating symlink to Python package directory"
+    ln -s /layers/savy_python/python/.local /home/cnb/.local
+fi
+
--- stacks/scripts/savy_lens_julia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Symlink Julia package directory
-if [ ! -L ~/.julia ]; then
-    echo "Creating symlink to Julia package directory"
-    ln -s /layers/savy_julia/julia/.julia /home/cnb/.julia
-fi
-    
