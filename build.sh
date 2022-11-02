552a4b09fbaa117ba67ea9e8901d2109410a1fda
Diego Ripley <diego@diegoripley.ca>
2022-11-02 17:35:39 +0000

    Enable buildpack.

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pack builder create test:jammy --config ./builders/jammy/builder.toml
 ## Julia
 pack build savy_lens_julia --builder test:jammy --path ./apps/SaVy.jl/
 ## Python
-#pack build savy_lens_python --builder test:jammy --path ./apps/SAVY_LENS_FASTAPI
+pack build savy_lens_python --builder test:jammy --path ./apps/SAVY_LENS_FASTAPI
